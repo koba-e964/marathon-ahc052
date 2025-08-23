832a134841c04396c7ca1c809184fb7815460dea
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2025-08-23 17:42:41 +0900

    Greedy 0: 727

--- a.rs
+++ a.rs
@@ -65,44 +65,98 @@ impl<T: PartialOrd> Change for T {
     fn chmin(&mut self, x: T) { if *self > x { *self = x; } }
 }
 
+fn try_move(
+    x: usize, y: usize,
+    v: &[Vec<char>], h: &[Vec<char>], dir: char,
+) -> Option<(usize, usize)> {
+    let n = 30;
+    let (nx, ny) = match dir {
+        'U' => (x.wrapping_sub(1), y),
+        'D' => (x.wrapping_add(1), y),
+        'L' => (x, y.wrapping_sub(1)),
+        'R' => (x, y.wrapping_add(1)),
+        'S' => (x, y),
+        _ => unreachable!(),
+    };
+    if nx >= n || ny >= n {
+        return None;
+    }
+    let is_blocked = match dir {
+        'U' => h[nx][y] == '1',
+        'D' => h[x][y] == '1',
+        'L' => v[x][ny] == '1',
+        'R' => v[x][y] == '1',
+        'S' => false,
+        _ => panic!(),
+    };
+    if is_blocked {
+        None
+    } else {
+        Some((nx, ny))
+    }
+}
+
 fn calc_bitboard(
     ij: &[(usize, usize)], v: &[Vec<char>], h: &[Vec<char>],
     alloc: &[Vec<char>], ops: &[usize],
-) -> Vec<u32> {
+) -> (Vec<u32>, Vec<(usize, usize)>) {
     let n = 30;
     let mut bitboard = vec![0; n];
+    let mut pts = vec![];
     for (i, row) in alloc.iter().enumerate() {
         let (mut x, mut y) = ij[i];
         for &o in ops {
             bitboard[x] |= 1 << y;
             let letter = row[o];
-            let (nx, ny) = match letter {
-                'U' => (x.wrapping_sub(1), y),
-                'D' => (x.wrapping_add(1), y),
-                'L' => (x, y.wrapping_sub(1)),
-                'R' => (x, y.wrapping_add(1)),
-                'S' => (x, y),
-                _ => unreachable!(),
-            };
-            if nx >= n || ny >= n {
-                continue;
-            }
-            let is_blocked = match letter {
-                'U' => h[nx][y] == '1',
-                'D' => h[x][y] == '1',
-                'L' => v[x][ny] == '1',
-                'R' => v[x][y] == '1',
-                'S' => false,
-                _ => panic!(),
-            };
-            if !is_blocked {
+            if let Some((nx, ny)) = try_move(x, y, v, h, letter) {
                 x = nx;
                 y = ny;
             }
         }
         bitboard[x] |= 1 << y;
+        pts.push((x, y));
     }
-    bitboard
+    (bitboard, pts)
+}
+
+fn calc_distance(
+    v: &[Vec<char>], h: &[Vec<char>],
+    bitboard: &[u32],
+) -> Vec<Vec<i32>> {
+    let n = 30;
+    let mut dist = vec![vec![1000; n]; n];
+    let mut que = VecDeque::new();
+    for i in 0..n {
+        for j in 0..n {
+            if (bitboard[i] >> j) & 1 == 0 {
+                que.push_back((0, i, j));
+            }
+        }
+    }
+    while let Some((d, x, y)) = que.pop_front() {
+        if dist[x][y] <= d {
+            continue;
+        }
+        dist[x][y] = d;
+        for &(nx, ny) in &[(x.wrapping_sub(1), y), (x + 1, y), (x, y.wrapping_sub(1)), (x, y + 1)] {
+            if nx >= n || ny >= n {
+                continue;
+            }
+            let is_blocked = if nx < x {
+                h[nx][y] == '1'
+            } else if nx > x {
+                h[x][y] == '1'
+            } else if ny < y {
+                v[x][ny] == '1'
+            } else {
+                v[x][y] == '1'
+            };
+            if !is_blocked && dist[nx][ny] > d + 1 {
+                que.push_back((d + 1, nx, ny));
+            }
+        }
+    }
+    dist
 }
 
 fn main() {
@@ -132,9 +186,21 @@ fn main() {
     }
     let mut ops = vec![0; 2 * n * n];
     for i in 0..2 * n * n {
-        ops[i] = rng.next() as usize % k;
+        let (now_bb, now_pts) = calc_bitboard(&ij, &v, &h, &alloc, &ops[..i]);
+        let dist = calc_distance(&v, &h, &now_bb);
+        let mut best = (1 << 30, 0);
+        for i in 0..k {
+            let mut sum = 0;
+            for j in 0..m {
+                let np = try_move(now_pts[j].0, now_pts[j].1, &v, &h, alloc[j][i]);
+                let np = np.unwrap_or(now_pts[j]);
+                sum += dist[np.0][np.1];
+            }
+            best = best.min((sum, i));
+        }
+        ops[i] = best.1;
     }
-    let bitboard = calc_bitboard(&ij, &v, &h, &alloc, &ops);
+    let (bitboard, _) = calc_bitboard(&ij, &v, &h, &alloc, &ops);
     let mut score = 0;
     for i in 0..n {
         score += bitboard[i].count_ones();
